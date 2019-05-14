172964717a19d5de2d855de34f40fbfb57de30c3
Nikostreet11 <nico.pollini@gmail.com>
2019-05-14 12:56:31 +0200

    Added a personalized greetings from TestClass

--- EGit Test/Source/TestFolder/TestClass.cpp
+++ EGit Test/Source/TestFolder/TestClass.cpp
@@ -1,15 +1,32 @@
 
 #include "TestClass.hpp"
 
+
+// Constructor / Destructor
 TestClass::TestClass()
 {
+	initVariables();
 }
 
 TestClass::~TestClass()
 {
 }
 
+// Methods
 void TestClass::testMethod()
 {
-	std::cout << "Hello from TestClass!" << std::endl;
+	std::cin >> name;
+
+	if (name != "")
+		std::cout << "Hello " << name << "! My name is TestClass :)" <<
+		std::endl;
+	else
+		std::cout << "Hello! My name is TestClass :)" << std::endl;
+
+}
+
+// Initialization
+void TestClass::initVariables()
+{
+	name = "";
 }
--- EGit Test/Source/TestFolder/TestClass.hpp
+++ EGit Test/Source/TestFolder/TestClass.hpp
@@ -3,14 +3,24 @@
 #define TESTFOLDER_TESTCLASS_HPP_
 
 #include <iostream>
+#include <string>
 
 class TestClass
 {
 public:
+	// Constructor / Destructor
 	TestClass();
 	virtual ~TestClass();
 
+	// Methods
 	void testMethod();
+
+private:
+	// Initialization
+	void initVariables();
+
+	// Variables
+	std::string name;
 };
 
 #endif /* TESTFOLDER_TESTCLASS_HPP_ */
