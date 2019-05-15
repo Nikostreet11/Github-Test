f5e7c4f6985637a14ed3b3a91f3f48939a6c3477
Nikostreet11 <nico.pollini@gmail.com>
2019-05-15 10:58:44 +0200

    TestClass now can say the today's date

--- EGit Test/Source/TestFolder/TestClass.cpp
+++ EGit Test/Source/TestFolder/TestClass.cpp
@@ -3,6 +3,7 @@
 
 TestClass::TestClass()
 {
+	initVariables();
 }
 
 TestClass::~TestClass()
@@ -11,5 +12,23 @@ TestClass::~TestClass()
 
 void TestClass::testMethod()
 {
-	std::cout << "Hello from TestClass!" << std::endl;
+	std::cout << "Hi, I am TestClass!" << std::endl;
+
+	setCurrentTime();
+
+    std::cout << "Today's date is " <<
+			(localtime->tm_mon + 1) << '/' <<
+			localtime->tm_mday << "/" <<
+    		(localtime->tm_year + 1900) << std::endl;
+}
+
+void TestClass::setCurrentTime()
+{
+	time = std::time(0);
+	localtime = std::localtime(&time);
+}
+
+void TestClass::initVariables()
+{
+	localtime = nullptr;
 }
--- EGit Test/Source/TestFolder/TestClass.hpp
+++ EGit Test/Source/TestFolder/TestClass.hpp
@@ -3,14 +3,26 @@
 #define TESTFOLDER_TESTCLASS_HPP_
 
 #include <iostream>
+#include <ctime>
 
 class TestClass
 {
 public:
+	// Constructor
 	TestClass();
+	// Destructor
 	virtual ~TestClass();
 
+	// Functions
 	void testMethod();
+	void setCurrentTime();
+
+private:
+	void initVariables();
+
+	// Variables
+	std::time_t time;
+	std::tm* localtime;
 };
 
 #endif /* TESTFOLDER_TESTCLASS_HPP_ */
